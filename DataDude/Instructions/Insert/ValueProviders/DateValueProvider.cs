11cf5189216782d8968d35e0a3b1fc72374b69e4
Carl Berg <carl@carl-berg.se>
2023-03-11 16:43:50 +0100

    Handling default values (including null) for most sql server data types

--- DataDude/Instructions/Insert/ValueProviders/DateValueProvider.cs
+++ DataDude/Instructions/Insert/ValueProviders/DateValueProvider.cs
@@ -10,6 +10,7 @@ namespace DataDude.Instructions.Insert.ValueProviders
             return column.DataType switch
             {
                 "date" or "datetime" or "datetime2" => new ColumnValue(new DateTime(1753, 1, 1, 12, 0, 0)),
+                "smalldatetime" => new ColumnValue(new DateTime(1900, 1, 1, 12, 0, 0)),
                 "datetimeoffset" => new ColumnValue(new DateTimeOffset(1753, 1, 1, 12, 0, 0, TimeSpan.Zero)),
                 "time" => new ColumnValue(TimeSpan.Zero),
                 _ => null
